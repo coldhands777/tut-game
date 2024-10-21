19eed2a29f745bd9ab95d617af1e211f246d6c56
lootf777 <lootfd@gmail.com>
2024-10-21 14:25:05 -0500

    added enemy movement and animations

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -16,10 +16,15 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	print ("you died")
+	body.get_node("AnimatedSprite2D").animation = "death"
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 	
 	
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,45 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	#flip sprite depending on direction
+	if direction > 0:
+		animated_sprite.flip_h = 0
+	elif direction == -1:
+		animated_sprite.flip_h = 1
+
+		
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.animation = "idle"
+		else:
+			animated_sprite.animation = "run"
+	else:
+		animated_sprite.animation = "jump"
+		
+		
+	# apply movement
 	if direction:
+		
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
